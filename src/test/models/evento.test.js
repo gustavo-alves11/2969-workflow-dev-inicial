4571d463801750b34538a43882834a701557b165
gustavo <gustavosalves09@gmail.com>
2024-08-07 18:45:32 -0300

    feat: :sparkles: add: modelo eventos e testes do modelo

---
+++ src/test/models/evento.test.js
@@ -0,0 +1,21 @@
+import {
+    describe, expect, it, jest,
+  } from '@jest/globals';
+import Evento from '../../models/evento.js';
+
+describe('Testando o modelo evento', () => {
+    const objetoevento = {
+      nome: 'evento teste',
+      descricao: 'Descrição do evento',
+      data: '2024-05-05',
+      autor_id: 1,
+    };
+  
+    it('Deve instanciar um novo evento', () => {
+      const evento = new Evento(objetoevento);
+  
+      expect(evento).toEqual(
+        expect.objectContaining(objetoevento),
+      );
+    });
+});
